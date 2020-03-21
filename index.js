e218781c60e10d7369d980ca72af039df3ece63b
Toyeeb Ganiu <tboynonilekan@gmail.com>
2020-03-21 21:20:33 +0100

    Javascript comit

---
+++ index.js
@@ -0,0 +1,13 @@
+const myName = "Toyeeb Ganiu";
+const courses = ["HTML", "CSS", "PHP", "JavaScript", "MySQL"];
+console.log(myName);
+
+for (let course of courses) {
+  console.log(course);
+}
+
+for (let i = 1; i <= 200; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
